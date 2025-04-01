f62641394dfdaeeb6f7f0c7e287fc08c5123ed48
arslanart <arslanart913@gmail.com>
2025-04-01 17:29:21 +0700

    ➕เพิ่มไฟล์ใหม่สำหรับ Summernote และ Inputmask plugins รวมถึงไฟล์ LICENSE และ README 📱

---
+++ resources/views/layouts/backend/menu.blade.php
@@ -0,0 +1,12 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4 table-responsive">
+    <nav class="mt-2 table-responsive">
+
+        <!-- Page Wrapper -->
+        <div id="wrapper">
+
+            <!-- Sidebar -->
+            <ul class="navbar-nav bg-dark sidebar sidebar-dark accordion table-responsive" id="accordionSidebar">
+                <hr class="sidebar-divider">
+            </ul>
+    </nav>
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,13 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::post('/login',[LoginController::class, 'loginPost'])->name('login'); // ส่งข้อมูลไปยัง LoginController
+Route::post('/login', [LoginController::class, 'loginPost'])->name('login'); // ส่งข้อมูลไปยัง LoginController
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified','check_user_type'
+    'verified',
+    'check_user_type'
 ])->group(function () {
     Route::get('/dashboard', [UserController::class, 'adminDashboard'])->name('dashboard'); // เส้นทาง Admin
     Route::get('/user-dashboard', [UserController::class, 'userDashboard'])->name('user-dashboard'); // เส้นทาง User
