dd24b3841283c142d3c8875542c59efa6cf0e159
kongzang <gongpipop.2546@gmail.com>
2025-04-02 11:19:40 +0700

    🌐ปรับปรุงเมนูด้านข้างในแดชบอร์ด backend โดยเพิ่มลิงก์สำหรับ Home, Group, Inventory, Alarm และ Administrator 📝

--- resources/views/layouts/backend/menu.blade.php
+++ resources/views/layouts/backend/menu.blade.php
@@ -7,6 +7,46 @@
             <!-- Sidebar -->
             <ul class="navbar-nav bg-dark sidebar sidebar-dark accordion table-responsive" id="accordionSidebar">
                 <hr class="sidebar-divider">
+
+                <a class="sidebar-brand d-flex align-items-center justify-content-center">
+                    <div class="sidebar-brand-icon">
+
+                    </div>
+                </a>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider my-3"> <!-- ปรับระยะห่างของเส้นแบ่ง -->
+
+                <!-- Nav Item - Dashboard -->
+                <li class="nav-item active">
+                    <a class="nav-link" href="">
+                        <i class="fas fa-fw fa-home"></i>
+                        <span>Home</span></a>
+                </li>
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="">
+                        <i class="fas fa-fw fa-table"></i>
+                        <span>Group</span></a>
+                </li>
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="">
+                        <i class="fas fa-fw fa-box"></i>
+                        <span>Inventory</span></a>
+                </li>
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="">
+                        <i class="fas fa-fw fa-bell"></i>
+                        <span>Alarm</span></a>
+                </li>
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="">
+                        <i class="fas fa-fw fa-male"></i>
+                        <span>Administrator</span></a>
+                </li>
             </ul>
     </nav>
 </aside>
