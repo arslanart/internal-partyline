51ce6de3a2e6a4a93ef32a5d77bd154adf66d7cc
arslanart <arslanart913@gmail.com>
2025-04-03 13:32:36 +0700

    🌐ปรับปรุงโลโก้ในเมนูด้านข้างของหน้า backend

--- resources/views/layouts/backend/menu.blade.php
+++ resources/views/layouts/backend/menu.blade.php
@@ -11,15 +11,11 @@
                 <hr class="sidebar-divider">
 
                 <a class="sidebar-brand d-flex align-items-center justify-content-center">
-<<<<<<< HEAD
                     <div class="sidebar-brand-icon"></div>
-=======
                     <div class="sidebar-brand-icon">
-                        <img src="asset/frontend/images/egatlogo.png" alt="logo"
-                                class="img-fluid"
+                        <img src="asset/frontend/images/egatlogo.png" alt="logo" class="img-fluid"
                             style="width: 200px; height: auto;">
                     </div>
->>>>>>> 498eb31336dc8b32cc5b53877fbb66b6c0c96cad
                 </a>
 
                 <hr class="sidebar-divider my-3">
