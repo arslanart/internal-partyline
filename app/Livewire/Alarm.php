9436e28abc597672d5483a00e029745dc497cd0a
kongzang <gongpipop.2546@gmail.com>
2025-04-03 11:04:30 +0700

    🌐เพิ่ม Livewire components สำหรับหน้า Administrator, Alarm และ Group พร้อมสร้าง views สำหรับการแสดงผล 📝

---
+++ app/Livewire/Alarm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Alarm extends Component
+{
+    public function render()
+    {
+        return view('livewire.alarm');
+    }
+}
