7f3cbbab9a3ceba1c9c966fc26e2bfbae1958bea
kongzang <gongpipop.2546@gmail.com>
2025-04-08 17:53:45 +0700

    🌐เพิ่มโครงสร้าง HTML สำหรับหน้า Home: สร้าง card layout พร้อม header

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div class="content-wrapper">
+        <div class="card shadow-sm">
+            <div class="card-header bg-white text-dark">
+                <h4 class="mb-0">Home</h4>
+            </div>
+            <div class="card-body">
+
+
+            </div>
+        </div>
+    </div>
+</div>
