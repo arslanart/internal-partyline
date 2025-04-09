8f1dfab95971d991a6a4defbdbf61391b4a2b42c
kongzang <gongpipop.2546@gmail.com>
2025-04-09 09:48:26 +0700

    🌐ปรับปรุงโครงสร้างและสไตล์ของเมนูด้านข้าง: เปลี่ยนพื้นหลังและสีของไอคอนให้เข้ากับธีม

--- resources/views/layouts/backend/menu.blade.php
+++ resources/views/layouts/backend/menu.blade.php
@@ -1,7 +1,7 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4 table-responsive">
+<aside class="main-sidebar sidebar-white-primary elevation-4 table-responsive">
     <nav class="mt-2 table-responsive">
         <div id="wrapper">
-            <ul class="navbar-nav bg-dark sidebar sidebar-dark accordion table-responsive" id="accordionSidebar">
+            <ul class="navbar-nav bg-white sidebar sidebar-dark accordion table-responsive" id="accordionSidebar">
                 <hr class="sidebar-divider">
 
                 <a class="sidebar-brand d-flex align-items-center justify-content-center">
@@ -12,41 +12,41 @@
                     </div>
                 </a>
 
-                <hr class="sidebar-divider my-3">
+                <hr class="sidebar-divider my-3 bg-dark">
 
                 <!-- Nav Items -->
                 <li class="nav-item">
-                    <a class="nav-link sidebar-link" href="{{ route('home') }}">
-                        <i class="fas fa-fw fa-home"></i>
+                    <a class="nav-link sidebar-link text-dark" href="{{ route('home') }}">
+                        <i class="fas fa-fw fa-home text-dark"></i>
                         <span>Home</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link sidebar-link" href="{{ route('group') }}">
-                        <i class="fas fa-fw fa-table"></i>
+                    <a class="nav-link sidebar-link text-dark" href="{{ route('group') }}">
+                        <i class="fas fa-fw fa-table text-dark"></i>
                         <span>Group</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link sidebar-link" href="{{ route('inventory') }}">
-                        <i class="fas fa-fw fa-box"></i>
+                    <a class="nav-link sidebar-link text-dark" href="{{ route('inventory') }}">
+                        <i class="fas fa-fw fa-box text-dark"></i>
                         <span>Inventory</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link sidebar-link" href="{{ route('alarm') }}">
-                        <i class="fas fa-fw fa-bell"></i>
+                    <a class="nav-link sidebar-link text-dark" href="{{ route('alarm') }}">
+                        <i class="fas fa-fw fa-bell text-dark"></i>
                         <span>Alarm</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link sidebar-link" href="{{ route('administrator') }}">
-                        <i class="fas fa-fw fa-male"></i>
+                    <a class="nav-link sidebar-link text-dark" href="{{ route('administrator') }}">
+                        <i class="fas fa-fw fa-male text-dark"></i>
                         <span>Administrator</span>
                     </a>
                 </li>
 
-                <hr class="sidebar-divider my-3">
+                <hr class="sidebar-divider my-3 bg-dark">
 
             </ul>
         </div>
